6011b8383b5296bcbb2597ad93f97cd6343771de
Jiraffe <74108675+Jraf5023@users.noreply.github.com>
2021-02-26 22:28:59 -0500

    Add files via upload
    This is the assignment for 2/26/2021.

--- Assignment.js
+++ Assignment.js
@@ -1,49 +1,18 @@
-//if and else statement
-if (age < 16) {
-  greeting = "Welcome to the driving course!";
-} else {
-  greeting = "Good evening, you are not permitted to take this course.";
+// named function, anonymous function, and method. 
+
+var x = function (a, b) {return a * b};  //Anonymous Function
+var z = x(4, 3);
+
+
+var namedSum = function sum (a, b) { // Named Function
+    return a + b;
 }
 
-//switch statement
-
-switch (new Month().getMonth()) {
-  case 0:
-    Month = "January";
-    break;
-  case 1:
-    Month = "February";
-    break;
-  case 2:
-     Month = "March";
-    break;
-  case 3:
-    Month = "April";
-    break;
-  case 4:
-    Month = "May";
-    break;
-  case 5:
-    Month = "June";
-    break;
-  case 6:
-    Month = "July";
-}
-
-//Ternary operator
-
-
-function getInfo(isRegistered) {
-    return (isRegistered ? 'Yes' : 'No');
-  }
-  
-  console.log(getInfo(true));
-  console.log(getInfo(false));
-  console.log(getInfo(null));
-
-  //Logical Operators
-
-const a = 10;
-const b = 5;
-
-console.log(a > 9 && b > 4.9);
+var Chair = {                                       // Method
+    Material: "Wood",
+    Height : "Five feet",
+    Width  : "2 1/2 feet",
+    chairInfo : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
